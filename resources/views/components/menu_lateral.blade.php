1064a302f4027bd90ac8d538a9883bf6fdb2fb18
Enma <medina.enma1234@gmail.com>
2025-06-08 17:28:57 -0400

    Se refactoriza el formulario de estudiantes para optimizar la captura de datos.
    Se amplían los formularios de registro y edición de estudiantes para recopilar datos más completos, incluyendo nuevos campos para condición, género, teléfono, dirección, origen e información detallada para estudiantes no locales. Se eliminan las columnas y la interfaz de usuario antiguas de "Condición" y se optimiza el diseño de la tabla. Se mejora la lógica de validación, las interacciones dinámicas del formulario y la consistencia de los formularios de detalle. Se actualizan las etiquetas para mayor claridad; por ejemplo, se cambia el nombre de "Lapso" a "Lapso Académico".
    
    Se mejora la usabilidad, se garantiza una mejor calidad de los datos y se prepara para una elaboración de perfiles de estudiantes más completa.

--- resources/views/components/menu_lateral.blade.php
+++ resources/views/components/menu_lateral.blade.php
@@ -41,7 +41,7 @@
                         <a href="{{ route('servicio') }}" class="nav-link dropdown-link">Servicios</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('lapso') }}" class="nav-link dropdown-link">Lapso</a>
+                        <a href="{{ route('lapso') }}" class="nav-link dropdown-link">Lapso Academico</a>
                     </li>
                 </ul>
             </li>
